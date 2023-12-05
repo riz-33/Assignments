cb7b358f3347890029def9ae371e2928f6e3df1c
rizwan <118995669+riz-33@users.noreply.github.com>
2023-12-06 00:56:39 +0500

    Java

---
+++ Java/Marks Table/app.js
@@ -0,0 +1,21 @@
+var city = prompt ("Enter Your City")
+
+if(city === "karachi")(
+    alert ("Welcome to city of lights")
+)
+
+else (city !== "Karachi")(
+    alert ("Valid for Karachi only")
+)
+
+
+var gender = prompt ("Enter Your Gender")
+
+if (gender === "male"){
+    alert ("Good Morning Sir")
+}
+
+if (gender === "female"){
+    alert ("Good Morning Ma'am")
+}
+
---
+++ Java/Practice/app.js
@@ -0,0 +1,18 @@
+alert ("Hello World");
+
+alert ("Error! Please enter a valid password.")
+
+alert ("Welcome to JS Land... \n Happy Coding!")
+
+
+var userName = "Rizwan33"
+alert (userName)
+
+var myName = "Muhammad Rizwan"
+alert (myName)
+
+
+var hello = "Hello"
+console.log(hello)
+
+
