20a87775b0dea3c3d0074b34e18fb54db8f88968
rizwan <118995669+riz-33@users.noreply.github.com>
2024-03-10 03:39:01 +0500

    update

--- Java/Chap17-20-Array&Loop/app.js
+++ Java/Chap17-20-Array&Loop/app.js
@@ -1,3 +1,18 @@
-for (var i = 1; i < 11; i++) {
-    console.log (i);
+// for (var i = 1; i < 11; i++) {
+//     console.log (i);
+// }
+
+
+// var num = prompt("Enter Number") 
+// var length = prompt ("Enter Legth")
+
+// for (i = 1; i <= length; i++) {
+//   console.log(num + " x " + i + " = " + num * i);
+// }
+
+
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+
+for (var i = 0; i < fruits.length; i++) {
+console.log (i + " " + fruits[i])
 }
