37cadc01828b86949e5b133de345b884ec177ae0
rizwan <118995669+riz-33@users.noreply.github.com>
2024-03-10 19:05:33 +0500

    new assignment

---
+++ Java/Chap21-25-StringMethod/app.js
@@ -0,0 +1,9 @@
+// var firstName = prompt ("Enter Your First Name");
+
+// var lastName = prompt ("Enter Your Last Name")
+
+// var fullName = (firstName + lastName);
+
+// console.log ("Welcome!" + firstName + " " + lastName);
+
+
