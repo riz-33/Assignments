9eb3ae56b560d74a4a6e239efe95e306ce1ea93c
rizwan <118995669+riz-33@users.noreply.github.com>
2024-03-17 04:06:07 +0500

    update

--- Java/Chap21-25-StringMethod/app.js
+++ Java/Chap21-25-StringMethod/app.js
@@ -17,7 +17,7 @@
 
 var message = ("Ali and Sami are best friends. They play cricket and football together");
 
-var newMessage = message.replace (/and/g , "&");
+var newMessage = message.replaceAll ("and" , "&");
 
 console.log (message);
 
