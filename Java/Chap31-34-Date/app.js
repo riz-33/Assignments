3cae8b0fae85449816b814c08f8d4a6ae4ef230a
rizwan <118995669+riz-33@users.noreply.github.com>
2024-03-21 00:38:28 +0500

    update

---
+++ Java/Chap31-34-Date/app.js
@@ -0,0 +1,9 @@
+var date = new Date ();
+
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+var day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ]
+
+console.log ("Today is " + day[date.getDay()]);
+
+
