5383fe51932042984f0cb57200b9d4f426253375
rizwan <118995669+riz-33@users.noreply.github.com>
2024-04-03 01:49:58 +0500

    update

--- Java/Chap31-34-Date/app.js
+++ Java/Chap31-34-Date/app.js
@@ -7,3 +7,12 @@ var day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ]
 console.log ("Today is " + day[date.getDay()]);
 
 
+var today = new Date ();
+
+var dob = new Date ("31 May,1993")
+
+var diff = today.getTime() - dob.getTime();
+
+var age = diff / (1000 * 60 * 60 * 24 * 30 * 12)
+
+console.log ("Your age is " + Math.round (age));
