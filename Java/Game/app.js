905ab7ed0de2de2e0e59f319db6997a63f0c36d8
rizwan <118995669+riz-33@users.noreply.github.com>
2024-04-24 01:11:57 +0500

    complete

--- Java/Game/app.js
+++ Java/Game/app.js
@@ -25,3 +25,32 @@ function abc() {
 }
 
 window.onkeydown = abc;
+
+// var forward2 = 1200;
+
+// function foo() {
+//     var character2 = document.getElementById('character2');
+//     console.log (event.keyCode)
+//     if (event.keyCode === 39) {
+//         forward2 = forward2 + 10;
+//         character2.style.left = forward2 + 'px'
+//         character2.src = 'Images/Player3-2.gif'
+//     }
+//     if (event.keyCode === 37) {
+//         forward2 = forward2 - 10;
+//         character2.style.left = forward2 + 'px'
+//         character2.src = 'Images/Player3-2.gif'
+//     }
+
+//     if (event.keyCode === 13) {
+//         character2.src = 'Images/Player3-3.gif'
+//         character2.style.width = '270px'
+//         setTimeout(function () {
+//             character2.src = 'Images/Player3-1.gif';
+//             character2.style.width = '150px'
+
+//         }, 2000)
+//     }
+// }
+
+// window.onkeydown = foo;
