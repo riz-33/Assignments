3a0e5b1ddb92a6ac770985541d65990fe9ca5452
rizwan <118995669+riz-33@users.noreply.github.com>
2024-05-01 02:26:56 +0500

    update

--- Java/PostApp/app.js
+++ Java/PostApp/app.js
@@ -1,7 +1,31 @@
-function addPost(){
-    var title = document.getElementById ("title");
-    var description = document.getElementById ("description")
+function addPost() {
+    var title = document.getElementById("title");
+    var description = document.getElementById("description")
 
-console.log(title.value , description.value)
+    if (title.value.trim() && description.value.trim()) {
+
+        var posts = document.getElementById("posts");
+        posts.innerHTML += `
+        <div class="card p-4">
+        <div class="card-header"> Post </div>
+        <div class="card-body">
+        <blockquote class="blockquote mb-1">
+        <p> ${title.value} </p>
+        <footer class="blockquote-footer"> ${description.value} </footer>
+        </blockquote>
+        </div>
+        </div>
+        `
+
+        title.value = "";
+        description.value = "";
+
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Please Enter Title & Description!",
+        });
+    }
 }
 
